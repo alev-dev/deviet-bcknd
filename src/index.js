77623bcd7739783dd0e4583e030cefdefbfc6d3c
OinsaneX <alejandroalfonsovargas@gmail.com>
2021-02-06 23:49:39 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const app = require("./app");
 require("./database");
 
 const main = () => {
-  app.listen(4000, () => {
+  app.listen(process.env.PORT || 4000, () => {
     console.log("server on port " + "4000");
   });
 };
